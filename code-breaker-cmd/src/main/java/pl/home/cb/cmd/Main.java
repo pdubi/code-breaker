7b0b586b9216f9eecf0a86e722cd746be6de7abc
pdubi <dubiela90@gmail.com>
2017-04-23 12:38:17 +0200

    Added story to actual execution

--- code-breaker-cmd/src/main/java/pl/home/cb/cmd/Main.java
+++ code-breaker-cmd/src/main/java/pl/home/cb/cmd/Main.java
@@ -4,9 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Type text!");
-        Scanner echo = new Scanner(System.in);
-        String echoText = echo.nextLine();
-        System.out.println(echoText);
+        Game game = new Game();
+        System.out.println(game.getWelcomeMessage());
     }
 }
