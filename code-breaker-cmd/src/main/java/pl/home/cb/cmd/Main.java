fc1bdef5912ed498069270251d4025df4c0294e7
pdubi <dubiela90@gmail.com>
2017-04-26 19:06:30 +0200

    Fixed compilation issue

--- code-breaker-cmd/src/main/java/pl/home/cb/cmd/Main.java
+++ code-breaker-cmd/src/main/java/pl/home/cb/cmd/Main.java
@@ -1,10 +1,13 @@
 package pl.home.cb.cmd;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Game game = new Game();
+        Random random = new Random();
+        String secret = String.format("%04d", random.nextInt(6000));
+        Game game = new Game(secret);
         System.out.println(game.getWelcomeMessage());
     }
 }
